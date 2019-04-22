b27ea52ad731251573ca14f458e0aed40a9adf3a
leonredman <leonproart@yahoo.com>
2019-04-21 19:38:12 -0700

    Updates

---
+++ precache-manifest.59db3b2a649501db6827c80aaa731d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "bbc8f074a2087f27ad22",
+    "url": "/react-gh-pages/static/js/main.4646d39e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-gh-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "bbc8f074a2087f27ad22",
+    "url": "/react-gh-pages/static/css/main.bef1a13c.chunk.css"
+  },
+  {
+    "revision": "cbc84ce85b626869c94fbd80609b020b",
+    "url": "/react-gh-pages/index.html"
+  }
+];
